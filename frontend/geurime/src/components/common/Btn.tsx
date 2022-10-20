e4e8b120e1b85c2ef7e7d346967b7e3aec74b3f8
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-10-20 13:37:51 +0900

    Feat: 의존성 추가

---
+++ frontend/geurime/src/components/common/Btn.tsx
@@ -0,0 +1,24 @@
+/*
+버튼
+속성: bgColor, fontColor, width
+@author 여예원
+@since 2022.10.20
+*/
+
+import * as React from 'react';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+
+export default function ContainedButtons() {
+  return (
+    <Stack direction="row" spacing={2}>
+      <Button variant="contained">Contained</Button>
+      <Button variant="contained" disabled>
+        Disabled
+      </Button>
+      <Button variant="contained" href="#contained-buttons">
+        Link
+      </Button>
+    </Stack>
+  );
+}
