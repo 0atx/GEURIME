9c2a2e7b889918c4af44fe45531940ee9940c229
Yunha <ha110011@naver.com>
2022-10-21 11:00:08 +0900

    Feat: 소셜 로그인 구현 완료
    카카오 및 구글 소셜 로그인 구현 완료했습니다.

--- backend/src/main/java/geurime/database/entity/User.java
+++ backend/src/main/java/geurime/database/entity/User.java
@@ -5,9 +5,12 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.DynamicUpdate;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
+import java.nio.channels.AsynchronousChannelGroup;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
@@ -15,6 +18,7 @@ import java.util.List;
 @Entity
 @Getter
 @DynamicUpdate
+@EntityListeners(AuditingEntityListener.class)
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Table(name = "user")
 public class User {
@@ -26,23 +30,27 @@ public class User {
     @Column(name = "email", nullable = false)
     private String email;
 
+    @Column(name = "provider", nullable = false)
+    private String provider;
+
     @Column(name = "refresh_token")
     private String refreshToken;
 
     @Column(name = "user_name", nullable = false)
     private String userName;
 
+    @CreatedDate
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     @Column(name = "create_date")
     private LocalDate createDate;
 
-    @Column(name = "nickname", nullable = false)
+    @Column(name = "nickname", unique = true)
     private String nickname;
 
     @Column(name = "user_profile_image")
     private String userProfileImage;
 
-    @Column(name = "simple_password", nullable = false, length = 6)
+    @Column(name = "simple_password", length = 6)
     private String simplePassword;
 
     @Column(name = "is_active", nullable = false)
@@ -54,10 +62,20 @@ public class User {
     @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true, fetch = FetchType.LAZY)
     private List<Board> boardList = new ArrayList<>();
 
+    public void setRefreshToken(String refreshToken) {
+        this.refreshToken = refreshToken;
+    }
+
+    @PrePersist
+    public void prePersist() {
+        this.isActive = true;
+    }
+
     @Builder
-    public User(Long id, String email, String refreshToken, String userName, LocalDate createDate, String nickname, String userProfileImage, String simplePassword, Boolean isActive, List<Kid> kidList, List<Board> boardList) {
+    public User(Long id, String email, String provider, String refreshToken, String userName, LocalDate createDate, String nickname, String userProfileImage, String simplePassword, Boolean isActive, List<Kid> kidList, List<Board> boardList) {
         this.id = id;
         this.email = email;
+        this.provider = provider;
         this.refreshToken = refreshToken;
         this.userName = userName;
         this.createDate = createDate;
@@ -68,4 +86,11 @@ public class User {
         this.kidList = kidList;
         this.boardList = boardList;
     }
+
+    public User update(String userName, String email) {
+        this.userName = userName;
+        this.email = email;
+        return this;
+    }
+
 }
---
+++ backend/src/main/java/geurime/database/enums/AuthAttributes.java
@@ -0,0 +1,46 @@
+package geurime.database.enums;
+
+import geurime.api.dto.auth.UserProfile;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.function.Function;
+
+public enum AuthAttributes {
+
+    GOOGLE("google", (attributes) -> {
+        UserProfile userProfile = new UserProfile();
+        userProfile.setName((String) attributes.get("name"));
+        userProfile.setEmail((String) attributes.get("email"));
+        return userProfile;
+    }),
+
+    KAKAO("kakao", (attributes) -> {
+        // kakao는 kakao_account에 유저정보가 있다. (email)
+        Map<String, Object> kakaoAccount = (Map<String, Object>) attributes.get("kakao_account");
+        // kakao_account안에 또 profile이라는 JSON객체가 있다. (nickname, profile_image)
+        Map<String, Object> kakaoProfile = (Map<String, Object>) kakaoAccount.get("profile");
+
+        UserProfile userProfile = new UserProfile();
+        userProfile.setName((String) kakaoProfile.get("nickname"));
+        userProfile.setEmail((String) kakaoAccount.get("email"));
+        return userProfile;
+    });
+
+    private final String registrationId;
+    private final Function<Map<String, Object>, UserProfile> of;
+
+    AuthAttributes(String registrationId, Function<Map<String, Object>, UserProfile> of) {
+        this.registrationId = registrationId;
+        this.of = of;
+    }
+
+    public static UserProfile extract(String registrationId, Map<String, Object> attributes) {
+        return Arrays.stream(values())
+                .filter(provider -> registrationId.equals(provider.registrationId))
+                .findFirst()
+                .orElseThrow(IllegalArgumentException::new)
+                .of.apply(attributes);
+    }
+
+}
