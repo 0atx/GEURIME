c80d94b0e78ef9bf5cb507f90cb3c1a5ad087c33
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-10-25 17:33:12 +0900

    Feat: userinfo.js 레이아웃

--- frontend/geurime/src/pages/home/RegistKids.js
+++ frontend/geurime/src/pages/home/RegistKids.js
@@ -6,23 +6,34 @@ export default function RegistKids() {
   const [kids, setKids] = useState([]);
 
   return (
-    <Grid container alignItems="center">
-      <Grid item sx={{ background: "red", textAlign: "center", height: "90vh", width: "100vw" }}>
+    <Grid id="container">
+      <Grid
+        direction="row"
+        container
+        alignItems="center"
+        sx={{
+          textAlign: "center",
+          height: "80vh",
+          width: "100vw",
+        }}
+      >
         {kids.length == 0 ? (
-          <>
+          <Grid item xs={12} sx={{ color: "#6f6f6f" }}>
             <Typography align="center" variant="h5">
               위쪽의 + 버튼을 눌러
             </Typography>
             <Typography align="center" variant="h5">
               아이 정보를 등록하세요!
             </Typography>
-          </>
+          </Grid>
         ) : (
           <></>
         )}
       </Grid>
       <Grid container justifyContent="flex-end">
-        <Btn sx={{ marginBottom: "5vh", marginTop: "3vh", marginRight: "1vh" }}>다음</Btn>
+        <Btn onClick={{}} sx={{ marginBottom: "5vh", marginTop: "3vh", marginRight: "1vh" }}>
+          다음
+        </Btn>
       </Grid>
     </Grid>
   );
