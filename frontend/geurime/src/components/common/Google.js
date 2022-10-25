62e25aed66477a69fc95e39ca8295d740b3862c1
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-10-25 17:04:45 +0900

    Feat: 서버 주소 변경

--- frontend/geurime/src/components/common/Google.js
+++ frontend/geurime/src/components/common/Google.js
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Google() {
   const GOOGLE_REDIRECT_URI = process.env.REACT_APP_GOOGLE_REDIRECT_URI;
-  const GOOGLE_REQUEST = `${GOOGLE_REDIRECT_URI}?redirect_uri=http://localhost:3000/oauth/redirect`;
-  // const KAKAO_REQUEST = `${KAKAO_REDIRECT_URI}?redirect_uri=http://서버주소/oauth/redirect`
+  // const GOOGLE_REQUEST = `${GOOGLE_REDIRECT_URI}?redirect_uri=http://localhost:3000/oauth/redirect`;
+  const GOOGLE_REQUEST = `${GOOGLE_REDIRECT_URI}?redirect_uri=http://k7a506.p.ssafy.io/oauth/redirect`;
 
   return (
     <>
