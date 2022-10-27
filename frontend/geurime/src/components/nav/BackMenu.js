6c2f8d107cb42e08ee1ef092f83547cc5a241170
chohyean <ashley3053@gmail.com>
2022-10-27 11:24:39 +0900

    Feat: 일기등록 백메뉴 완료

--- frontend/geurime/src/components/nav/BackMenu.js
+++ frontend/geurime/src/components/nav/BackMenu.js
@@ -1,6 +1,6 @@
 /*
 BackMenu 헤더
-속성 : isLeft(Boolean), title(String), clickTitle(타이틀클릭 함수), isRight(String), clickRight(오른쪽클릭 함수)
+속성 : type(String), isLeft(Boolean), title(String), clickTitle(타이틀클릭 함수), isRight(String), clickRight(오른쪽클릭 함수)
 @author 조혜안
 @since 2022.10.25
 */
@@ -11,8 +11,9 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import { Grid } from "@mui/material";
 import KeyboardArrowLeftRoundedIcon from "@mui/icons-material/KeyboardArrowLeftRounded";
+import ArrowDropDownRoundedIcon from "@mui/icons-material/ArrowDropDownRounded";
 
-export default function BackMenu({ isLeft, title, clickTitle, isRight, clickRight }) {
+export default function BackMenu({ type, isLeft, title, clickTitle, isRight, clickRight }) {
   return (
     <Box
       sx={{
@@ -47,6 +48,7 @@ export default function BackMenu({ isLeft, title, clickTitle, isRight, clickRigh
                 onClick={clickTitle}
               >
                 {title}
+                {type === "registDiary" && <ArrowDropDownRoundedIcon></ArrowDropDownRoundedIcon>}
               </span>
             </Grid>
             {/* isRight */}
