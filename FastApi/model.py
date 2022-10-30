0cc25182e394a67627f2e9172eea7681479e2e27
gks3075 <97646749+gks3075@users.noreply.github.com>
2022-10-30 23:59:31 +0900

    Feat : model test

---
+++ FastApi/model.py
@@ -0,0 +1,25 @@
+import tensorflow as tf
+import numpy as np
+import cv2
+import imageio
+import cv2
+# from tensorflow.keras.preprocessing import image
+
+model_path = 'efficientnet_v2_s_childdata.h5'
+# model = load_model(model_path)
+model = tf.keras.models.load_model(model_path)
+train_input_shape = (224, 224, 3)
+labels = ['depression', 'violence', 'happiness']
+url = 'https://bananabearbooks.com/wp-content/uploads/2018/04/tiger-illustration-bananabeabooks.jpg'
+
+web_image = imageio.imread(url)
+web_image = cv2.resize(web_image, dsize=train_input_shape[0:2], )
+# web_image = image.img_to_array(web_image)
+web_image = tf.keras.preprocessing.image.img_to_array(web_image)
+web_image /= 255.
+web_image = np.expand_dims(web_image, axis=0)
+prediction = model.predict(web_image)
+prediction_probability = np.amax(prediction)
+prediction_idx = np.argmax(prediction)
+emotion = labels[prediction_idx]
+print({"emotion": emotion, "prediction": prediction_probability})
