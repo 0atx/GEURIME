ea2c41a28665530c2b9df83acc81072a8a7325f9
유현욱 <hyundnr1126@naver.com>
2022-10-31 17:06:30 +0900

    Feat: Board.js 레이아웃 완료

--- frontend/geurime/src/pages/board/Board.js
+++ frontend/geurime/src/pages/board/Board.js
@@ -3,19 +3,24 @@
 @since 2022.10.28
 */
 import * as React from 'react';
-import { Grid } from "@mui/material";
+import { Grid, InputAdornment, InputBase, Paper, TextField } from "@mui/material";
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
 import BackMenu from "components/nav/BackMenu";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import BoardInputItem from "./BoardInputItem";
+import HighlightOffIcon from "@mui/icons-material/HighlightOff";
 import InputLabel from '@mui/material/InputLabel';
 import MenuItem from '@mui/material/MenuItem';
+import SearchIcon from '@mui/icons-material/Search';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
 
 export default function Board() { 
   const [test, setTest] = useState();
   const [list, setList] = useState('');
-
+  const [searchKeyWord, setSearchKeyWord] = useState();
+  const searchInput = useRef(null); // 검색바 input 객체
   const handleChange = (event) => {
     setList(event.target.value);
   };
@@ -23,7 +28,7 @@ export default function Board() {
   useEffect(() => {
     setTest(  [{
       "boardCategory": "string",
-      "boardFirstImage": "string",
+      "boardFirstImage": "https://en.pimg.jp/031/716/685/1/31716685.jpg",
       "boardId": 0,
       "boardTitle": "1번글",
       "boardViews": 0,
@@ -36,7 +41,7 @@ export default function Board() {
     },
     {
       "boardCategory": "string",
-      "boardFirstImage": "string",
+      "boardFirstImage": "https://en.pimg.jp/031/716/685/1/31716685.jpg",
       "boardId": 1,
       "boardTitle": "2번글",
       "boardViews": 0,
@@ -50,9 +55,17 @@ export default function Board() {
   },[])
   useEffect(() => {
     console.log({테스트: test})
-  },[test])
+  }, [test])
+  async function search(e) {
+    if (e.key === "Enter") {
+      e.preventDefault();
+      console.log("검색");
+      searchInput.current.value = "";
+    }
+  }
+
   return (
-    <div>
+    <Grid>
       {/* <Grid>
       <BackMenu
           isLeft={true}
@@ -62,32 +75,104 @@ export default function Board() {
       </BackMenu>
       </Grid> */}
       
-      <Grid>
-      <FormControl sx={{ m: 1, minWidth: 120 }}>
+      <Grid
+        container
+        direction="row"
+        justifyContent="center"
+        alignItems="flex-end"
+        flexWrap={'nowrap'}
+        marginLeft='5%'
+      >
+      <FormControl sx={{ minWidth: '25%' }}>
         <Select
           value={list}
           onChange={handleChange}
           displayEmpty
-          inputProps={{ 'aria-label': 'Without label' }}
+            inputProps={{ 'aria-label': 'Without label' }}
+
         >
-          <MenuItem value="">
+            <MenuItem value={"제목"}>
             <em>제목</em>
           </MenuItem>
           <MenuItem value={10}>Ten</MenuItem>
           <MenuItem value={20}>Twenty</MenuItem>
           <MenuItem value={30}>Thirty</MenuItem>
         </Select>
-      </FormControl>
+        </FormControl>
+      {/* 검색바 영역 */}
+      <Grid
+        container
+        direction="row"
+        style={{ padding: "2vw" }}
+        alignItems="center"
+          justifyContent="center"
+          marginLeft={'5%'}
+      >
+          <Grid 
+            container
+            direction="row"
+            justifyContent='space-between'
+          >
+          <Paper
+            elevation={0}
+            component="form"
+              id="search"
+          >
+            {/* 검색어 입력 부분 */}
+            <Input
+              placeholder="검색어를 입력하세요..."
+              onChange={(e) => {
+                setSearchKeyWord(e.target.value);
+              }} // 검색 키워드 변경
+              id="searchValue"
+              inputRef={searchInput} // input 객체를 반환
+              onKeyDown={(e) => {
+                // search(e);
+                }} // enter시 검색하는 함수
+            />
+            {/* 검색어 삭제 버튼 */}
+            {searchKeyWord == null || searchKeyWord == "" ? null : (
+              <IconButton
+                type="button"
+                sx={{ p: "8px" }}
+                aria-label="search"
+                onClick={() => {
+                  searchInput.current.value = null; // input 객체의 값을 비운다.
+                  setSearchKeyWord(null);
+                }}
+              >
+                <HighlightOffIcon />
+              </IconButton>
+            )}
+            </Paper>
+            <Grid>
+            {/* 돋보기 버튼 */}
+            <IconButton
+              type="button"
+              sx={{ color: "#FFA000" }}
+              aria-label="search"
+              onClick={(e) => {
+                // searchClick(e);
+              }}
+            >
+              <SearchIcon />
+              </IconButton>
+            </Grid>
+        </Grid>
       </Grid>
-      <Grid>
-        {test?.map((store, idx) => {
-          return <BoardInputItem key={idx} item={store}/>;
-        })}
-      </Grid>
+
     {/* <div>
       <h1>{test[0].boardTitle}</h1>
     </div> */}
-    </div>
+      </Grid>
+      <Grid
+        height={'25%'}
+      >
+      {test?.map((store, idx) => {
+        return <BoardInputItem key={idx} item={store}/>;
+      })}
+    </Grid>
+    </Grid>
   )
 
 } 
--- frontend/geurime/src/pages/board/BoardInputItem.js
+++ frontend/geurime/src/pages/board/BoardInputItem.js
@@ -1,11 +1,90 @@
 import { Grid, Paper } from "@mui/material";
+import { useEffect } from "react";
+import Visibility from '@mui/icons-material/Visibility';
+import QuestionAnswerOutlinedIcon from '@mui/icons-material/QuestionAnswerOutlined';
+import { fontWeight } from "@mui/system";
 
-export default function BoardInputItem({item}) { 
+
+export default function BoardInputItem({ item }) { 
+  useEffect(() => {
+    console.log({아이템: item})
+  }, [])
+  const time = item.createTime.substring(0,10);
   return (
-    <div>
-    <Paper>
+    <Paper
+      style={{ margin: '3%', marginBottom: '5%' }}
+      variant="outlined"
+      sx={{
+        borderColor: '#FFE082',
+        borderWidth: 3
+      }}
       
-    </Paper>
-    </div>
+    >
+      {/* 제목 부분 */}
+      <Grid
+        sx={{
+          margin: '4%',
+          fontWeight:"bold"
+        }}
+      >
+        {item.boardTitle}
+      </Grid>
+      
+      <Grid
+        container
+        sx={{alignItems: 'center', display: 'flex'}}
+        direction="row"
+        flexWrap={'nowrap'}
+      >
+        <Grid
+          item
+          sx={{ justifyContent: 'center', textAlign: 'center' }}
+          xs={8}
+          
+        >
+        <img
+          src={item.boardFirstImage}
+          loading="lazy"
+          style={{ height: '20vh', width: '25vh', margin: '3%' }}
+          />
+
+      </Grid>
+
+        <Grid
+          justifyContent="center"
+          sx={{ marginTop: '3%'}}
+        >
+          <Grid>
+            작성자 : {item.userNickname}
+         </Grid>
+          <Grid
+            sx={{ marginTop: '6%' }}
+          >
+            작성일 : {time}
+          </Grid>
+          <Grid
+            sx={{ marginTop: '6%'}}
+            justifyContent="center"
+          >
+            <Grid
+                container
+                direction="row"
+            >
+              <Grid
+                sx={{marginRight: '10%'}}
+              >
+              <Visibility fontSize="8"/>
+                  {item.boardViews}
+              </Grid>
+              <Grid>
+              <QuestionAnswerOutlinedIcon fontSize="8" />
+                {item.commentCount}
+              </Grid>
+          </Grid>
+          </Grid>
+          
+        </Grid>
+      </Grid>
+  </Paper>
   )
 }
