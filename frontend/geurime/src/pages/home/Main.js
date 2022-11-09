7cd81661f3f0ece9d2a1fe9761fc536ffc197298
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-11-09 17:55:23 +0900

    Feat: 가족정보

--- frontend/geurime/src/pages/home/Main.js
+++ frontend/geurime/src/pages/home/Main.js
@@ -1,5 +1,5 @@
 import React, { useState, useRef, useEffect } from "react";
-import { Grid } from "@mui/material";
+import { Avatar, AvatarGroup, Grid, Paper } from "@mui/material";
 import Masonry from "@mui/lab/Masonry";
 import { useNavigate } from "react-router-dom";
 import NavBar from "components/nav/NavBar";
@@ -12,11 +12,47 @@ export default function Main() {
   // 유저 state
   const [userInfo, setUserInfo] = useRecoilState(userState);
 
+  // 가족 정보
+  const [familyInfo, setFamilyInfo] = useState([
+    {
+      nickname: "예원1",
+      userid: 0,
+      userProfileImage: "",
+    },
+    {
+      nickname: "예원2",
+      userid: 0,
+      userProfileImage: "",
+    },
+    {
+      nickname: "예원3",
+      userid: 0,
+      userProfileImage: "",
+    },
+    {
+      nickname: "예원4",
+      userid: 0,
+      userProfileImage: "",
+    },
+    {
+      nickname: "예원5",
+      userid: 0,
+      userProfileImage: "",
+    },
+  ]);
+
   // 처음 로딩시 유저정보 가져오기
   async function getUserInfo() {
     const response = await http.get(`/users/${userInfo.userId}`);
     if (response.data.message === "success") {
       setUserInfo(response.data.data);
+    }
+  }
+
+  async function getFamilyInfo() {
+    const response = await http.get(`/users/family/${userInfo.userId}`);
+    if (response.data.message === "success") {
+      setFamilyInfo(response.data.data);
     }
   }
 
@@ -26,6 +62,7 @@ export default function Main() {
       mounted.current = true;
     } else {
       getUserInfo();
+      getFamilyInfo();
     }
   }, []);
 
@@ -41,6 +78,55 @@ export default function Main() {
         justifyContent="center"
         alignItems="center"
       >
+        <Grid item xs={10} sx={{ fontSize: "2.5vh", marginBottom: "1vh" }}>
+          가족 정보
+        </Grid>
+        <Grid
+          container
+          sx={{
+            marginBottom: "3vh",
+            width: "85%",
+            background: "white",
+            borderRadius: "5px",
+            boxShadow: "1px 1px 3px #9e9e9e",
+            padding: "1vh",
+          }}
+          justifyContent="center"
+          alignItems="center"
+        >
+          {familyInfo.map(function (info, i) {
+            return (
+              <Grid
+                item
+                key={i}
+                sx={{
+                  textAlign: "center",
+                  justifyContent: "center",
+                  marginBottom: "10px",
+                }}
+              >
+                <Grid
+                  container
+                  justifyContent="center"
+                  sx={{
+                    textAlign: "center",
+                    width: "70px",
+                  }}
+                >
+                  <Avatar src={info.userProfileImage} />
+
+                  <Grid item xs={12}>
+                    {info.nickname.length > 4 ? (
+                      <>{info.nickname.substring(0, 3) + ".."}</>
+                    ) : (
+                      <>{info.nickname}</>
+                    )}
+                  </Grid>
+                </Grid>
+              </Grid>
+            );
+          })}
+        </Grid>
         <Grid item xs={10} sx={{ fontSize: "2.5vh" }}>
           그림 갤러리
         </Grid>
