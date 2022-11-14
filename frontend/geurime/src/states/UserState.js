86337809ff1a96c403bf3d746941741f692cce79
여예원 <98465383+YewonYeo@users.noreply.github.com>
2022-11-14 10:22:46 +0900

    Fix: board.js 오류 수정

--- frontend/geurime/src/states/UserState.js
+++ frontend/geurime/src/states/UserState.js
@@ -13,8 +13,8 @@ import { atom } from "recoil";
 export const userState = atom({
   key: "user",
   default: {
-    // userId: localStorage.getItem("userId"),
-    userId: 62,
+    userId: localStorage.getItem("userId"),
+    // userId: 62,
     userName: localStorage.getItem("name"),
     nickname: "",
     email: localStorage.getItem("email"),
