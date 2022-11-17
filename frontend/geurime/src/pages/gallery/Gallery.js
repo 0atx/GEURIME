0c861663b35d10805f5d37311968a23b43a30126
chohyean <ashley3053@gmail.com>
2022-11-17 14:10:15 +0900

    Feat: skeleton 구현

--- frontend/geurime/src/pages/gallery/Gallery.js
+++ frontend/geurime/src/pages/gallery/Gallery.js
@@ -17,6 +17,7 @@ import {
   ImageListItem,
   ImageListItemBar,
   IconButton,
+  Skeleton,
 } from "@mui/material";
 import { Container } from "@mui/system";
 import { useNavigate } from "react-router-dom";
@@ -54,136 +55,162 @@ export default function Gallery() {
     setBoxes(response.data.data.drawingBoxDtoList);
   }
 
+  const [loading, setLoading] = useState(true);
+
   const mounted = useRef(false);
   useEffect(() => {
     if (!mounted.current) {
       mounted.current = true;
     } else {
-      getKidInfo();
-      getLikeBox();
+      setLoading(true);
+      setTimeout(() => {
+        getKidInfo();
+        getLikeBox();
+        setLoading(false);
+      }, 1000);
     }
   }, [open]);
 
   return (
     <Grid>
-      <BackMenu
-        title={`${currentKid.kidName}의 갤러리`}
-        type="registKids"
-        clickRight={modalOpen}
-      />
+      <BackMenu title={`${currentKid.kidName}의 갤러리`} type="registKids" clickRight={modalOpen} />
       <Grid id="container">
         <RegistDrawingBoxModal open={open} setOpen={setOpen} />
         <Container>
-          <Grid
-            container
-            rowSpacing={2}
-            columnSpacing={{ xs: 2, sm: 3, md: 4 }}
-            textAlign="center"
-          >
-            {likeBox.length !== 0 ? (
-              <Grid
-                item
-                xs={6}
-                sm={4}
-                md={2}
-                onClick={() => {
-                  navigater("/detailgallery/0");
-                }}
-              >
-                <Paper
-                  elevation={4}
-                  sx={{
-                    width: "20vh",
-                    height: "20vh",
-                    borderRadius: "20px",
+          {loading ? (
+            <Grid
+              container
+              rowSpacing={2}
+              columnSpacing={{ xs: 2, sm: 3, md: 4 }}
+              textAlign="center"
+            >
+              {[0, 1, 2, 3].map((item, i) => (
+                <Grid item xs={6} sm={4} md={2}>
+                  <Skeleton
+                    key={i}
+                    sx={{ borderRadius: "20px" }}
+                    variant="rectangular"
+                    width="20vh"
+                    height="20vh"
+                  />
+                  <Typography sx={{ marginTop: "1vh" }} variant="body1">
+                    <Skeleton />
+                  </Typography>
+                </Grid>
+              ))}
+            </Grid>
+          ) : (
+            <Grid
+              container
+              rowSpacing={2}
+              columnSpacing={{ xs: 2, sm: 3, md: 4 }}
+              textAlign="center"
+            >
+              {likeBox.length !== 0 ? (
+                <Grid
+                  item
+                  xs={6}
+                  sm={4}
+                  md={2}
+                  onClick={() => {
+                    navigater("/detailgallery/0");
                   }}
                 >
-                  <ImageListItem
-                    style={{
+                  <Paper
+                    elevation={4}
+                    sx={{
                       width: "20vh",
                       height: "20vh",
                       borderRadius: "20px",
                     }}
                   >
-                    <img
-                      src={likeBox[0].drawingImagePath}
-                      width="100%"
-                      height="100%"
+                    <ImageListItem
                       style={{
+                        width: "20vh",
+                        height: "20vh",
                         borderRadius: "20px",
-                        objectFit: "cover",
                       }}
-                    />
-                    <ImageListItemBar
-                      sx={{
-                        background: "rgba(0,0,0,0)",
-                      }}
-                      position="top"
-                      actionIcon={
-                        <IconButton sx={{ color: "#FF5252" }}>
-                          <FavoriteIcon sx={{ fontSize: "30px" }} />
-                        </IconButton>
-                      }
-                    />
-                  </ImageListItem>
-                </Paper>
-                <Typography sx={{ marginTop: "1vh" }} variant="body1">
-                  좋아요 보관함 ({likeBox.length})
-                </Typography>
-              </Grid>
-            ) : null}
-            {boxes.map((box, i) => (
-              <Grid
-                item
-                xs={6}
-                sm={4}
-                md={2}
-                key={i}
-                onClick={() => {
-                  navigater(`/detailgallery/${box.drawingBoxId}`);
-                }}
-              >
-                <Paper
-                  elevation={4}
-                  sx={{
-                    width: "20vh",
-                    height: "20vh",
-                    borderRadius: "20px",
+                    >
+                      <img
+                        src={likeBox[0].drawingImagePath}
+                        width="100%"
+                        height="100%"
+                        style={{
+                          borderRadius: "20px",
+                          objectFit: "cover",
+                        }}
+                      />
+                      <ImageListItemBar
+                        sx={{
+                          background: "rgba(0,0,0,0)",
+                        }}
+                        position="top"
+                        actionIcon={
+                          <IconButton sx={{ color: "#FF5252" }}>
+                            <FavoriteIcon sx={{ fontSize: "30px" }} />
+                          </IconButton>
+                        }
+                      />
+                    </ImageListItem>
+                  </Paper>
+                  <Typography sx={{ marginTop: "1vh" }} variant="body1">
+                    좋아요 보관함 ({likeBox.length})
+                  </Typography>
+                </Grid>
+              ) : null}
+              {boxes.map((box, i) => (
+                <Grid
+                  item
+                  xs={6}
+                  sm={4}
+                  md={2}
+                  key={i}
+                  onClick={() => {
+                    navigater(`/detailgallery/${box.drawingBoxId}`);
                   }}
                 >
-                  {box.drawingCount === 0 ? (
-                    <img
-                      src="assets/sample/1.png"
-                      width="100%"
-                      height="100%"
-                      style={{
-                        borderRadius: "20px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  ) : (
-                    <img
-                      src={box.thumbnailImage}
-                      width="100%"
-                      height="100%"
-                      style={{
-                        borderRadius: "20px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  )}
-                </Paper>
-                <Typography sx={{ marginTop: "1vh" }} variant="body1">
-                  {box.drawingBoxName.length > 8 ? (
-                    <>{box.drawingBoxName.substring(0, 7) + ".."}</>
-                  ) : (
-                    <>{box.drawingBoxName}</>
-                  )}
-                  ({box.drawingCount})
-                </Typography>
-              </Grid>
-            ))}
-          </Grid>
+                  <Paper
+                    elevation={4}
+                    sx={{
+                      width: "20vh",
+                      height: "20vh",
+                      borderRadius: "20px",
+                    }}
+                  >
+                    {box.drawingCount === 0 ? (
+                      <img
+                        src="assets/sample/1.png"
+                        width="100%"
+                        height="100%"
+                        style={{
+                          borderRadius: "20px",
+                          objectFit: "cover",
+                        }}
+                      />
+                    ) : (
+                      <img
+                        src={box.thumbnailImage}
+                        width="100%"
+                        height="100%"
+                        style={{
+                          borderRadius: "20px",
+                          objectFit: "cover",
+                        }}
+                      />
+                    )}
+                  </Paper>
+                  <Typography sx={{ marginTop: "1vh" }} variant="body1">
+                    {box.drawingBoxName.length > 8 ? (
+                      <>{box.drawingBoxName.substring(0, 7) + ".."}</>
+                    ) : (
+                      <>{box.drawingBoxName}</>
+                    )}
+                    ({box.drawingCount})
+                  </Typography>
+                </Grid>
+              ))}
+            </Grid>
+          )}
         </Container>
         {/* 하단 네비 */}
         <NavBar />
