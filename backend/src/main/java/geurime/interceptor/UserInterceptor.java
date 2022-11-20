62ca910a4106ebdc49bb5ea9746f714491b246e0
shinzan7 <97588187+shinzan7@users.noreply.github.com>
2022-11-20 16:54:00 +0900

    Fix : 같은 가족의 프로필 조회가능하도록 변경

--- backend/src/main/java/geurime/interceptor/UserInterceptor.java
+++ backend/src/main/java/geurime/interceptor/UserInterceptor.java
@@ -1,6 +1,7 @@
 package geurime.interceptor;
 
 import geurime.config.jwt.JwtService;
+import geurime.database.entity.Family;
 import geurime.database.entity.User;
 import geurime.database.repository.DrawingRepository;
 import geurime.database.repository.UserRepository;
@@ -41,12 +42,13 @@ public class UserInterceptor implements HandlerInterceptor {
         User userJwt = userRepository.findByEmailAndProvider(email, provider)
                 .orElseThrow(() -> new CustomException(CustomExceptionList.USER_NOT_FOUND_ERROR));
 
-        Long requestUserId = userJwt.getId();
+        Family familyJwt = userJwt.getFamily();
 
         User userRequest = userRepository.findById(Long.parseLong(userIdString))
                 .orElseThrow(() -> new CustomException(CustomExceptionList.USER_NOT_FOUND_ERROR));
+        Family familyRequest = userRequest.getFamily();
 
-        if(requestUserId != null && requestUserId == userRequest.getId()){
+        if(familyRequest != null && familyJwt != null && familyRequest.getId() == familyJwt.getId()){
             return true;
         }else{
             throw new CustomException(CustomExceptionList.NO_AUTHENTICATION_ERROR);
